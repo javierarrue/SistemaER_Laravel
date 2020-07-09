1360a4aa76452b907b4b895c22069a7a435d0de0
AdminMath <61955749+AdminMath@users.noreply.github.com>
2020-07-09 12:31:57 -0600

    Update C_AgregarProfesor.php
    aqui se agrega el profesor

--- app/Http/Controllers/C_AgregarProfesor.php
+++ app/Http/Controllers/C_AgregarProfesor.php
@@ -9,4 +9,30 @@ class C_AgregarProfesor extends Controller
     public function Index(){
         return view('AgregarProfesor');
     }
+    
+    public function Store(R_AgregarProfesor $request){
+        
+        
+         $ced=$request['cedula'];
+         
+         
+      $bro=$this->cedul($ced);
+      
+
+ 
+         if(1==$bro){
+
+            
+             $saved = ingresar_profesores::create($request->all());
+             
+             $this->idactualizar($ced, $saved);
+           
+             return redirect('MostrarProfesores')->with('status', 'El profesor se ingreso correctamente');
+           } else {
+            
+            return redirect()->back()->withInput()->withErrors('EL PROFESOR COMO USUARIO NO ESTA REGISTRADO');
+          }
+   
+         
+     }
 }
