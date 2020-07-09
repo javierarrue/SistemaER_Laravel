c4c345864a51d55aaff97caebca3622767948246
AdminMath <61955749+AdminMath@users.noreply.github.com>
2020-07-09 13:35:14 -0600

    Add files via upload
    este controller es para que cuando el usuario este en la vista mostrar profesores pueda clickear el boton de registrar profesores y se enviara a la vista de AgregarProfesor2 para que cuando apriete el boton volver vuelva a la pagina donde se muestran los profesores y no a la vista de crear lista de profesores

---
+++ app/Http/Controllers/C_AgregarProfesor2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class C_AgregarProfesor2 extends Controller
+{
+    public function AgregarProfesor2(){
+        return view('AgregarProfesor2');
+    }
+}
